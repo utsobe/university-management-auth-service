9379803946d03ecdf401cb9cd6ca49580b4e8389
utsobe <mmhutsobe01@gmail.com>
2023-10-21 17:27:15 +0800

    test

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,15 @@ import cors from 'cors'
 
 const app: Application = express()
 
-        app.use(cors())
+app.use(cors())
 
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Testing
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: any, res: any) => {
   res.send('Working successfully!')
-});
+})
 
 export default app
