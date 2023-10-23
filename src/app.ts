3fd1622b46d3fa73a36bb966935cb54513b060e9
utsobe <mmhutsobe01@gmail.com>
2023-10-23 16:10:57 +0800

    [DONE] improve linting setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
 import usersRouter from './app/modules/users/users.route'
 
 const app: Application = express()
